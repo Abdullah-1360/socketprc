c7b1a4db2f21a816a7f25fe34079103aa0e02780
Abdullah-1360 <abdullah.shah78963@gmail.com>
2025-07-14 21:58:41 +0500

    addded

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ const http= require('http');
 const server= http.createServer(app);
 const {Server}= require('socket.io');
 const io= new Server(server);
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);    
+    });
+});
